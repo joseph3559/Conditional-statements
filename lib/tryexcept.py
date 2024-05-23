328b13869765307a00ea055892fb3351bee733fa
joseph3559 <scottjoe3559@gmail.com>
2024-05-23 04:41:26 +0300

    16 Passed

---
+++ lib/tryexcept.py
@@ -0,0 +1,14 @@
+def divide(num1, num2):
+    try:
+        quotient = num1 / num2
+        print(quotient)
+    except ZeroDivisionError:
+        print("Error: num2 cannot be equal to 0")
+    except TypeError:
+        print("Error: input must be of type int or float")
+    finally:
+        print("Isn't division fun?")
+
+# Testing the function with different arguments
+divide(10, 2)
+
